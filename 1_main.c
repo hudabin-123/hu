29303ed788cf5441444d4e44e550026d0865ebc0
hudabin-123 <1749919461@qq.com>
2025-10-22 20:19:18 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,12 @@
-//
-//
-//
+//202510302225
+//1749919461@qq.com
+//胡大彬
 #include <stdio.h>
 int main()
 {
-    printf("Hello World");
+    float a =0;
+    scanf("%f",&a);
+    float b=a*9/5+32;
+    printf ("%.1f" , b);
     return 0;
 }
