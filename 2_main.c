d0965542891d719207a08f205c3e24c80352e56f
hudabin-123 <1749919461@qq.com>
2025-10-22 21:28:39 +0800

    Create 2_main.c

---
+++ 2_main.c
@@ -0,0 +1,21 @@
+//202510302225
+//179919461@qq.com
+//胡大彬
+#include <stdio.h>
+ int main() {
+     int score;
+     printf("请输入学生的整数成绩（0 - 100）：");
+     scanf("%d", &score);
+     if (score >= 90 && score <= 100) {
+         printf("A\n");
+     } else if (score >= 80 && score <= 89) {
+         printf("B\n");
+     } else if (score >= 70 && score <= 79) {
+         printf("C\n");
+     } else if (score >= 60 && score <= 69) {
+         printf("D\n");
+     } else {
+         printf("E\n");
+     }
+     return 0;
+    }
