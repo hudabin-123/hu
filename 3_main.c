240b7fdbc4aed6d7d89190b5e0edd43ab18cab4a
hudabin-123 <1749919461@qq.com>
2025-11-01 12:19:08 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -2,23 +2,25 @@
 //1749919461@qq.com
 //胡大彬
 #include <stdio.h>
-int main(){
-    int a,b;
-    char op;
-    scanf("%d %d %c",&a,&b,&op);
-    switch(op){
-        case'+':
-        printf("%d\n",a+b);
-        break;
-        case'-':
-        printf("%d\n",a-b);
-        break;
-        case'*':
-        printf("%d\n",a*b);
-        break;
-        case'/':
-        printf("%d\n",a/b);
-        break;
-    }
-return 0;
-}
+ int main() {
+     int num = 100, digit1, digit2, digit3;
+     int count = 0;
+     
+     // 用while循环遍历100到999
+     while (num <= 999) {
+         digit1 = num / 100;
+         digit2 = (num / 10) % 10;
+         digit3 = num % 10;
+         
+         if (digit1*digit1*digit1 + digit2*digit2*digit2 + digit3*digit3*digit3 == num) {
+             if (count > 0) {
+                 printf(" ");
+             }
+             printf("%d", num);
+             count++;
+         }
+         num++; // 循环变量自增
+     }
+     printf("\n");
+     return 0;
+ }
